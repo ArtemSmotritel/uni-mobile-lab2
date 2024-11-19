aa9b88ef34501fc963464dfc68cfefd4d5add53f
Artem Smotrytel <artemsmotritel@gmail.com>
2024-11-19 21:26:24 +0200

    fix build error

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.smo_uni_mobile_lab2"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.smo_uni_mobile_lab2"
